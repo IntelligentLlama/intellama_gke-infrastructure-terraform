557f0cc2cd0d14cf11d29c872dac42ad50cd2c6e
IntelligentLlama <72282458+IntelligentLlama@users.noreply.github.com>
2021-12-21 19:03:31 -0500

    Update gke.sh

--- docker-autodeployGKE/gke.sh
+++ docker-autodeployGKE/gke.sh
@@ -1,8 +1,8 @@
 #!/bin/ash
-git clone https://github.com/dpizar/dpizar_gke-infrastructure-terraform.git
+git clone https://github.com/IntelligentLlama/intellama_gke-infrastructure-terraform.git
 
-cd dpizar_gke-infrastructure-terraform/run-modules/
-cp -r /terraformfiles/auth/ /terraformfiles/dpizar_gke-infrastructure-terraform/run-modules/
+cd intellama_gke-infrastructure-terraform/run-modules/
+cp -r /terraformfiles/auth/ /terraformfiles/intellama_gke-infrastructure-terraform/run-modules/
 
 ls -a
 ls auth/ -a
